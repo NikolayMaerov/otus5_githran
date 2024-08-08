ed4f8050bfad152743feea7705ca606b80f9d3a4
Администратор <@localhost>
2024-08-08 15:13:41 +0400

    Создание хранилища конфигурации

---
+++ gitsync_run.cmd
@@ -0,0 +1,7 @@
+## Инициализация гитсинха (один раз)
+
+gitsync init -u gitbot C:\otus\StorageTest_Git\ C:\Otus\otus5_githran\src\cf
+
+## Синхронизация с хранилищем
+
+gitsync sync -u gitbot C:\otus\StorageTest_Git\ C:\Otus\otus5_githran\src\cf
